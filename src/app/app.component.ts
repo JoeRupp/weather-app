855d6ef59dc925dcfd108687e23badfc9bccae49
Joe Rupp <joerpp8@gmail.com>
2023-02-24 17:12:39 -0700

    Add app initial structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit, OnDestroy {
+  cities = ["London", "Paris", "Moscow", "New York", "Karachi", "Sydney"];
+
+  cityControl!: FormControl;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.cityControl = new FormControl("")
+    this.cityControl.valueChanges.subscribe(value => this.router.navigate(value))
+  }
+
+  ngOnDestroy(): void {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import {MatSelectModule} from '@angular/material/select'; 
 
+// const routes: Routes = [
+//   {
+//     path: "",
+//     component: WeatherReportComponent
+//   },
+//   {
+//     path: ":locationName",
+//     component: WeatherReportComponent
+//   }
+// ];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
